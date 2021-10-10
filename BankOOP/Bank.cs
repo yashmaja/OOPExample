f70aaa0257665c8cb1063cd406ae2d044a74355b
Yashashree Majalikar <yashmaja@gmail.com>
2021-10-09 23:15:35 -0400

    lecture example

---
+++ BankOOP/Bank.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankOOP
+{
+    class Bank
+    {
+        //properties
+        private double Money;
+        public double money
+        {
+            get { return Money; }
+        }
+        private double Interest;
+
+        //constructor
+        public Bank()
+        {
+            Money = 0;
+            Interest = 0.05;
+        }
+
+        public Bank(double money)
+        {
+            Money = money;
+            Interest = 0.05;
+        }
+
+        public Bank(double money, double interest)
+        {
+            Money = money;
+            Interest = interest;
+        }
+
+        //methods
+        public void Deposit(double value)
+        {
+            Money += value;
+            CollectInterest();
+        }
+
+        public double Withdraw(double value)
+        {
+            if (value <= Money)
+            {
+                Money -= value;
+            }
+            else
+            {
+                Console.WriteLine("That was too much money. Try again.");
+                value = 0;
+            }
+
+            CollectInterest();
+            return value;
+        }
+
+        private void CollectInterest()
+        {
+            Money = Money * (1 + Interest);
+        }
+    }
+}
---
+++ BankOOP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BankOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank myBank = new Bank(100);
+            double x = myBank.money;
+
+            Console.WriteLine(myBank.money);
+            myBank.Deposit(35);
+            Console.WriteLine(myBank.money);
+            double cash = myBank.Withdraw(100000);
+            Console.WriteLine($"I am holding {cash} after withdrawal. ");
+            Console.WriteLine(myBank.money);
+        }
+    }
+}
